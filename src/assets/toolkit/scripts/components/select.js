e7a2c1653d7287f5c7d46de23ff8759ab0ebf47b
aleks_rdvn <aleks.rdvn@gmail.com>
2017-09-21 12:36:23 +0200

    select tag repaired

--- src/assets/toolkit/scripts/components/select.js
+++ src/assets/toolkit/scripts/components/select.js
@@ -4,32 +4,41 @@ $.fn.madCustomSelect = function() {
 
   return this.each(function() {
 
-    const select = $(this).find('select'),
-      title = $(this).children('.select__title'),
-      list = $(this).children('.select__list');
+    const $this = $(this),
+      $select = $this.find('select'),
+      $title = $this.children('.select__title'),
+      $list = $this.children('.select__list'),
+      $win = $('body');
 
     // select items to list items
-    for (let i = 0, len = select.children('option').length; i < len; i++) {
-      list.append('<li data-value="' + select.children('option').eq(i).val() + '">' + select.children('option').eq(i).text() + '</li>');
+    for (let i = 0, len = $select.children('option').length; i < len; i++) {
+      $list.append('<li data-value="' + $select.children('option').eq(i).val() + '">' + $select.children('option').eq(i).text() + '</li>');
     }
 
     // open list
-    title.on('click', function() {
-      list.toggle();
+    $title.on('click', function() {
+      $list.toggle();
       $(this).toggleClass('active');
     });
 
     // selected option
-    list.on('click', 'li', function() {
+    $list.on('click', 'li', function() {
       const val = $(this).data('value'),
         text = $(this).text();
-      title.text(text);
-      list.toggle();
-      select.val(val ? val : text);
-      title.toggleClass('active');
+      $title.text(text);
+      $list.toggle();
+      $select.val(val ? val : text);
+      $title.toggleClass('active');
       return false;
     });
 
+    // close list
+    $win.on('click', (e) => {
+      if ($this.has(e.target).length === 0 && !$this.is(e.target)) {
+        $list.hide();
+      }
+      return false;
+    });
   });
 
 };
